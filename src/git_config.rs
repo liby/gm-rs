856be223fce1409d0b5773b732e90ea3812fc43d
Bryan Lee <doslby@gmail.com>
2022-01-09 18:36:12 +0800

    feat: :sparkles: generate user collection from git config

--- src/git_config.rs
+++ src/git_config.rs
@@ -1,12 +1,20 @@
 pub mod config {
-    use std::path::{Path, PathBuf};
-
     use configparser::ini::Ini;
+    use std::fs;
+    use std::path::{Path, PathBuf};
 
     struct Opts {
         cwd: PathBuf,
         opt_type: String,
     }
+
+    #[derive(Debug)]
+    pub struct GitUser {
+        name: String,
+        email: String,
+        scope: String,
+    }
+
     pub fn get() -> std::io::Result<PathBuf> {
         let opts = Opts {
             cwd: std::env::current_dir()?,
@@ -40,11 +48,49 @@ pub mod config {
         }
     }
 
-    pub fn parse_config() -> std::io::Result<()> {
+    pub fn parse_config() -> Result<Vec<GitUser>, Box<dyn std::error::Error>> {
         let mut config = Ini::new();
-        let path = get()?;
-        let map = config.load(path);
-        println!("{:#?}", map);
-        Ok(())
+        config.load(get()?)?;
+        let sections = config.sections();
+        let includeif_sections = sections
+            .iter()
+            .filter(|section| section.starts_with("includeif"))
+            .collect::<Vec<&String>>();
+        let mut other_config_vec = Vec::new();
+        for section in includeif_sections {
+            let mut other_config = Ini::new();
+            let path_str = config.get(section, "path").unwrap();
+
+            let path = expand_tilde(&path_str).unwrap();
+
+            other_config.load(path)?;
+            other_config_vec.push(GitUser {
+                name: other_config.get("user", "name").unwrap(),
+                email: other_config.get("user", "email").unwrap(),
+                scope: section.to_owned(),
+            });
+        }
+
+        Ok(other_config_vec)
+    }
+
+    fn expand_tilde<P: AsRef<Path>>(path_user_input: P) -> Option<PathBuf> {
+        let p = path_user_input.as_ref();
+        if !p.starts_with("~") {
+            return Some(p.to_path_buf());
+        }
+        if p == Path::new("~") {
+            return dirs::home_dir();
+        }
+        dirs::home_dir().map(|mut h| {
+            if h == Path::new("/") {
+                // Corner case: `h` root directory;
+                // don't prepend extra `/`, just drop the tilde.
+                p.strip_prefix("~").unwrap().to_path_buf()
+            } else {
+                h.push(p.strip_prefix("~/").unwrap());
+                h
+            }
+        })
     }
 }
