ca385da4ce7b49b4699dab1935f92f593c83ec03
Bryan Lee <doslby@gmail.com>
2022-01-13 23:51:23 +0800

    feat: :sparkles: add usage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod git_config;
 
+pub use crate::git_config::config;
+
+use clap::{App, AppSettings, Arg};
 use term_table::{
     row::Row,
     table_cell::{Alignment, TableCell},
     TableBuilder, TableStyle,
 };
-
-pub use crate::git_config::config;
 
 fn main() -> Result<(), std::io::Error> {
     let mut table_rows = vec![Row::new(vec![
@@ -28,7 +29,63 @@ fn main() -> Result<(), std::io::Error> {
         .rows(table_rows)
         .build();
 
-    println!("{}", table.render());
+    let matches = App::new("gum")
+        .bin_name("gum")
+        .version("0.1.0")
+        .about("Git multiple user config manager")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(App::new("list").about("List all the user config group"))
+        .subcommand(
+            App::new("set")
+                .about("Set one group for user config")
+                .arg(Arg::new("GROUP NAME").takes_value(true).required(true))
+                .arg(
+                    Arg::new("name")
+                        .long("name")
+                        .takes_value(true)
+                        .help("User name"),
+                )
+                .arg(
+                    Arg::new("email")
+                        .long("email")
+                        .takes_value(true)
+                        .help("User email"),
+                ),
+        )
+        .subcommand(
+            App::new("use")
+                .about("Use one group name for user config")
+                .arg(
+                    Arg::new("GROUP NAME")
+                        .takes_value(true)
+                        .conflicts_with("global"),
+                )
+                .arg(
+                    Arg::new("global")
+                        .long("global")
+                        .takes_value(false)
+                        .help("Git global config"),
+                ),
+        )
+        .subcommand(
+            App::new("delete")
+                .about("Delete one group")
+                .arg(Arg::new("GROUP NAME").takes_value(true).required(true)),
+        )
+        .get_matches();
 
+    match matches.subcommand() {
+        Some(("list", _arg)) => {
+            println!("{}", table.render());
+        }
+
+        Some(("set", _arg)) => {}
+
+        Some(("use", _arg)) => {}
+
+        Some(("delete", _arg)) => {}
+
+        _ => println!("Please use the help command to see the available commands"),
+    }
     Ok(())
 }
