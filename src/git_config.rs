01f29c172a6b1503cf85b24d5e74438ad1c3fd5e
Bryan Lee <doslby@gmail.com>
2022-01-14 23:53:04 +0800

    refactor: :recycle: optimize the code

--- src/git_config.rs
+++ src/git_config.rs
@@ -1,99 +1,115 @@
-pub mod config {
-    use configparser::ini::Ini;
-    use std::path::{Path, PathBuf};
+use configparser::ini::Ini;
+use std::io::Result;
+use std::path::{Path, PathBuf};
+use term_table::{
+    row::Row,
+    table_cell::{Alignment, TableCell},
+    TableBuilder, TableStyle,
+};
 
-    struct Opts {
-        cwd: PathBuf,
-        opt_type: String,
-    }
+pub struct GitUser {
+    pub name: String,
+    pub email: String,
+    pub scope: String,
+}
+pub struct GitUserCollection {
+    data: Vec<GitUser>,
+}
 
-    #[derive(Debug)]
-    pub struct GitUser {
-        pub name: String,
-        pub email: String,
-        pub scope: String,
-    }
+impl GitUserCollection {
+    pub fn new() -> GitUserCollection {
+        let global_config = Self::get_global_config().unwrap();
+        let global_sections = global_config.sections();
+        let mut config_vec = vec![GitUser {
+            name: global_config.get("user", "name").unwrap(),
+            email: global_config.get("user", "email").unwrap(),
+            scope: String::from("global"),
+        }];
 
-    pub fn get() -> std::io::Result<PathBuf> {
-        let opts = Opts {
-            cwd: std::env::current_dir()?,
-            opt_type: String::from("global"),
-        };
-
-        let mut config_path = if opts.opt_type == "global" {
-            Path::new(&dirs::home_dir().unwrap()).join(".gitconfig")
-        } else {
-            let mut path = PathBuf::from(opts.cwd);
-            path.push(".git/config");
-            path
-        };
-
-        if !config_path.exists() {
-            if opts.opt_type == "global" {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::NotFound,
-                    "No git config found",
-                ));
-            }
-            config_path = Path::new(&dirs::home_dir().unwrap()).join(".config/git/config")
-        }
-
-        match config_path.exists() {
-            true => Ok(config_path),
-            false => Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "No git config found",
-            )),
-        }
-    }
-
-    pub fn parse_config() -> Result<Vec<GitUser>, Box<dyn std::error::Error>> {
-        let mut config = Ini::new();
-        config.load(get()?)?;
-        let sections = config.sections();
-        let includeif_sections = sections
+        let includeif_sections = global_sections
             .iter()
             .filter(|section| section.starts_with("includeif"))
             .collect::<Vec<&String>>();
-        let mut other_config_vec = vec![GitUser {
-            name: config.get("user", "name").unwrap(),
-            email: config.get("user", "email").unwrap(),
-            scope: String::from("global"),
-        }];
+
         for section in includeif_sections {
             let mut other_config = Ini::new();
-            let path_str = config.get(section, "path").unwrap();
-
+            let path_str = global_config.get(section, "path").unwrap();
             let path = expand_tilde(&path_str).unwrap();
 
-            other_config.load(path)?;
-            other_config_vec.push(GitUser {
+            other_config.load(path).unwrap();
+            config_vec.push(GitUser {
                 name: other_config.get("user", "name").unwrap(),
                 email: other_config.get("user", "email").unwrap(),
                 scope: section.to_owned(),
             });
         }
 
-        Ok(other_config_vec)
+        GitUserCollection { data: config_vec }
     }
 
-    fn expand_tilde<P: AsRef<Path>>(path_user_input: P) -> Option<PathBuf> {
-        let p = path_user_input.as_ref();
-        if !p.starts_with("~") {
-            return Some(p.to_path_buf());
+    pub fn list(self) {
+        let mut table_rows = vec![Row::new(vec![
+            TableCell::new_with_alignment("Scope", 1, Alignment::Center),
+            TableCell::new_with_alignment("Name", 1, Alignment::Center),
+            TableCell::new_with_alignment("Email", 1, Alignment::Center),
+        ])];
+
+        for user in self.data {
+            table_rows.push(Row::new(vec![
+                TableCell::new(&user.scope),
+                TableCell::new_with_alignment(&user.name, 1, Alignment::Center),
+                TableCell::new(&user.email),
+            ]));
         }
-        if p == Path::new("~") {
-            return dirs::home_dir();
-        }
-        dirs::home_dir().map(|mut h| {
-            if h == Path::new("/") {
-                // Corner case: `h` root directory;
-                // don't prepend extra `/`, just drop the tilde.
-                p.strip_prefix("~").unwrap().to_path_buf()
-            } else {
-                h.push(p.strip_prefix("~/").unwrap());
-                h
-            }
-        })
+
+        let table = TableBuilder::new()
+            .style(TableStyle::extended())
+            .rows(table_rows)
+            .build();
+
+        println!("{}", table.render());
     }
+
+    pub fn get_global_config() -> Result<Ini> {
+        let config_path = Path::new(&dirs::home_dir().unwrap()).join(".gitconfig");
+
+        if !config_path.exists() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "No git config found",
+            ));
+        }
+
+        let mut global_config = Ini::new();
+        match config_path.exists() {
+            true => {
+                let _load = global_config.load(config_path);
+                Ok(global_config)
+            }
+            false => Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "No git config found",
+            )),
+        }
+    }
+}
+
+fn expand_tilde<P: AsRef<Path>>(path_user_input: P) -> Option<PathBuf> {
+    let p = path_user_input.as_ref();
+    if !p.starts_with("~") {
+        return Some(p.to_path_buf());
+    }
+    if p == Path::new("~") {
+        return dirs::home_dir();
+    }
+    dirs::home_dir().map(|mut h| {
+        if h == Path::new("/") {
+            // Corner case: `h` root directory;
+            // don't prepend extra `/`, just drop the tilde.
+            p.strip_prefix("~").unwrap().to_path_buf()
+        } else {
+            h.push(p.strip_prefix("~/").unwrap());
+            h
+        }
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,7 @@
 mod git_config;
-
-pub use crate::git_config::config;
-
 use clap::{App, AppSettings, Arg};
-use term_table::{
-    row::Row,
-    table_cell::{Alignment, TableCell},
-    TableBuilder, TableStyle,
-};
 
 fn main() -> Result<(), std::io::Error> {
-    let mut table_rows = vec![Row::new(vec![
-        TableCell::new_with_alignment("Scope", 1, Alignment::Center),
-        TableCell::new_with_alignment("Name", 1, Alignment::Center),
-        TableCell::new_with_alignment("Email", 1, Alignment::Center),
-    ])];
-
-    for user in config::parse_config().unwrap() {
-        table_rows.push(Row::new(vec![
-            TableCell::new(&user.scope),
-            TableCell::new_with_alignment(&user.name, 1, Alignment::Center),
-            TableCell::new(&user.email),
-        ]));
-    }
-
-    let table = TableBuilder::new()
-        .style(TableStyle::extended())
-        .rows(table_rows)
-        .build();
-
     let matches = App::new("gum")
         .bin_name("gum")
         .version("0.1.0")
@@ -74,9 +47,10 @@ fn main() -> Result<(), std::io::Error> {
         )
         .get_matches();
 
+    let config = git_config::GitUserCollection::new();
     match matches.subcommand() {
         Some(("list", _arg)) => {
-            println!("{}", table.render());
+            config.list();
         }
 
         Some(("set", _arg)) => {}
